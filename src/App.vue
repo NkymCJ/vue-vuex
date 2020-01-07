362d44d053bc199b19d70299894d3baa14f6946f
chenjian <nkymcj@outlook.com>
2020-01-07 18:18:26 +0800

    modified

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    {{ a }}
+    {{ count }}
+    {{ todos }}
+  </div>
 </template>
 
 <script>
-
+import { mapState } from 'vuex'
 export default {
-  name: 'app'
+  name: 'app',
+  computed: {
+    a () {
+      return '1'
+    },
+    ...mapState({
+      count: state => state.count,
+      todos: state => state.todos
+    })
+  }
 }
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,22 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    count: 0,
+    todos: [{
+      id: 1,
+      title: 'todo item 1',
+      completed: false
+    }, {
+      id: 2,
+      title: 'todo item 2',
+      completed: false
+    }, {
+      id: 3,
+      title: 'todo item 3',
+      completed: false
+    }]
+  },
+  getters: {
   },
   mutations: {
   },
